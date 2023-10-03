d19b1ad086c663033b2bb2c7d415c270b2ed219c
Aroz7894 <andrew.rozniakowski@gmail.com>
2023-10-03 00:04:43 -0400

    added updated apache code

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from generate_file_pandas import generate_output_file
+#from generate_file_apache import generate_output_w_beam
 
 
 
@@ -6,3 +7,4 @@ if __name__ == "__main__":
     dataset1_path = 'resources/dataset1.csv'
     dataset2_path = 'resources/dataset2.csv'
     generate_output_file(dataset1_path, dataset2_path)
+    # generate_output_w_beam(dataset1_path, dataset2_path)
